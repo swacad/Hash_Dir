339c49fc7df9bb4519d82d3faae6a41a14668d43
swacad <swacad@users.noreply.github.com>
2016-10-24 10:52:43 -0400

    Added SHA-1 support.

--- linux_hashing.sh
+++ linux_hashing.sh
@@ -1,1 +1,3 @@
 find / -type f -size +1 -exec md5sum -b {} \; > hashes.txt
+# For SHA1 format hashes use line below by commenting out top line and uncommenting bottom line.
+# find / -type f -size +1 -exec md5sum -b {} \; > hashes.txt
